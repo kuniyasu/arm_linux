afeb3c1ebcace1313501d383dd72c0fe7a6be905
Kuniyasu <kuniyasu@basil.ocn.ne.jp>
2015-08-19 20:23:40 +0900

    ADD gcc installation function.
    FAIL

--- create_root_fs.bash
+++ create_root_fs.bash
@@ -2,11 +2,16 @@
 
 PREFIX=$PWD
 TARGET=arm-unknown-linux-gnueabihf
+MAKE_OPT=-j8
 
 BINUTILS_VER=2.25
 BINUTILS_FILE=binutils-$BINUTILS_VER
 
+GCC_VER=5.2.0
+GCC_FILE=gcc-$GCC_VER
+
 PATH=/home/arm_cross_tools/bin:$PATH
+LD_LIBRARY_PATH=$PREFIX/usr/lib:$LD_LIBRARY_PATH
 
 function download(){
   if [ ! -f $2 ]; then
@@ -20,6 +25,10 @@ function create_build_dir(){
   fi
 
   cd $PREFIX/build
+
+  if [ -d $1 ]; then
+    rm -rf $1
+  fi
 
   if [ -d build-$1 ]; then
     rm -rf build-$1
@@ -38,12 +47,13 @@ function download_file(){
   cd download
 
   download http://ftp.gnu.org/gnu/binutils $BINUTILS_FILE.tar.gz
-
+  download http://ftp.gnu.org/gnu/gcc/$GCC_FILE $GCC_FILE.tar.gz
   cd ..
 }
 
 function install_binutils(){
   create_build_dir $BINUTILS_FILE
+
   cd build
   tar zxf ../download/$BINUTILS_FILE.tar.gz
 
@@ -71,5 +81,41 @@ function install_binutils(){
   cd $PREFIX
 }
 
+function install_gcc(){
+  create_build_dir $GCC_FILE
+
+  cd build
+
+  tar zxf ../download/$GCC_FILE.tar.gz
+  cd $GCC_FILE
+    ./contrib/download_prerequisites
+  cd ..
+
+  cd build-$GCC_FILE
+
+    CC=$TARGET-gcc \
+    CXX=$TARGET-g++ \
+    AR=$TARGET-ar \
+    LD=$TARGET-g++ \
+    RANLIB=$TARGET-ranlib \
+    ../$GCC_FILE/configure \
+    --prefix=$PREFIX/usr \
+    --with-local-prefix=$PREFIX/usr \
+    --host=$TARGET \
+    --with-native-system-header-dir=$PREFIX/usr/include \
+    --enable-languages=c,c++ \
+    --disable-libstdcxx-pch \
+    --disable-multilib \
+    --disable-bootstrap
+
+    make $MAKE_OPT
+
+    make install
+  cd ..
+
+  cd ..
+}
+
 download_file
 install_binutils
+install_gcc
